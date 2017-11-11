a41c7ea6b1369f3ddb8b36eba163406e1e4b5cbf
1ilsang <1ilsang@naver.com>
2017-11-11 15:46:01 +0900

    req Api

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
 
 /* GET home page. */
 router.get('/dump', function(req, res, next) {
@@ -7,4 +8,32 @@ router.get('/dump', function(req, res, next) {
         "data":"Hello"
     })
 });
+
+router.get('/go', function(req, res, next) {
+    request({
+        method: 'GET',
+        uri: 'http://sandbox.api.simsimi.com/request.p?key=3c832757-ca67-45d4-94c1-2bbcadb975f7&lc=ko&ft=1.0&text=너는 바보구나'
+      },
+      function (error, response, body) {
+        if (error) {
+          return console.error('upload failed:', error);
+        }
+        console.log('Upload successful!  Server responded with:', body);
+      });
+//    
+//    
+//    var options = {
+//      url: 'http://sandbox.api.simsimi.com/request.p?key=3c832757-ca67-45d4-94c1-2bbcadb975f7&lc=ko&ft=1.0&text=너는 바보구나',
+//      method: 'GET',
+//        headers: {
+//        'User-Agent': 'request'
+//      }
+//    };
+//    
+//    request(options, function(error, response, body) {
+//        console.log(body);
+//          res.json(body);
+//    });
+});
+
 module.exports = router;
