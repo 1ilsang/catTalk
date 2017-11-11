4c2708c15b55d3a4d249f009d3d03b8dc877d0e3
1ilsang <1ilsang@naver.com>
2017-11-11 16:36:53 +0900

    simsimi com

--- routes/api.js
+++ routes/api.js
@@ -9,31 +9,28 @@ router.get('/dump', function(req, res, next) {
     })
 });
 
-router.get('/go', function(req, res, next) {
-    request({
+router.get('/go/:msg', function(req, res, next) {
+    var options = {
         method: 'GET',
-        uri: 'http://sandbox.api.simsimi.com/request.p?key=3c832757-ca67-45d4-94c1-2bbcadb975f7&lc=ko&ft=1.0&text=너는 바보구나'
-      },
-      function (error, response, body) {
+        url: 'http://sandbox.api.simsimi.com/request.p',
+        qs:
+            { key: '3c832757-ca67-45d4-94c1-2bbcadb975f7',
+                text: req.params.msg,
+                lc: 'ko',
+                ft: '1.0' },
+        headers:
+            {
+                'cache-control': 'no-cache'
+            }
+    }
+    request(options, function (error, response, body) {
         if (error) {
           return console.error('upload failed:', error);
         }
         console.log('Upload successful!  Server responded with:', body);
+        console.log(response.headers);
+        res.send(body);
       });
-//    
-//    
-//    var options = {
-//      url: 'http://sandbox.api.simsimi.com/request.p?key=3c832757-ca67-45d4-94c1-2bbcadb975f7&lc=ko&ft=1.0&text=너는 바보구나',
-//      method: 'GET',
-//        headers: {
-//        'User-Agent': 'request'
-//      }
-//    };
-//    
-//    request(options, function(error, response, body) {
-//        console.log(body);
-//          res.json(body);
-//    });
 });
 
 module.exports = router;
