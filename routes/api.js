bdcc84c76a91ca79b1cb30bee9459e5328554027
1ilsang <1ilsang@naver.com>
2017-11-11 17:25:05 +0900

    finalServer

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var request = require('request');
+var naverToken = require('../config/naverToken');
 
 /* GET home page. */
 router.get('/dump', function(req, res, next) {
@@ -9,7 +10,32 @@ router.get('/dump', function(req, res, next) {
     })
 });
 
+router.get('/trans/:msg',function(req,res,next){
+   var query = req.params.msg;
+   var api_url = 'https://openapi.naver.com/v1/language/translate';
+   var request = require('request');
+   var options = {
+       url: api_url,
+       form: {'source':'ko', 'target':'en', 'text':query},
+       headers: {'X-Naver-Client-Id':naverToken.id, 'X-Naver-Client-Secret': naverToken.pwd}
+    };
+   request.post(options, function (error, response, body) {
+     if (!error && response.statusCode == 200) {
+       res.writeHead(200, {'Content-Type': 'text/json;charset=utf-8'});
+       res.end(body);
+     } else {
+       res.status(response.statusCode).end();
+       console.log('error = ' + response.statusCode);
+     }
+   });
+});
+
 router.get('/go/:msg', function(req, res, next) {
+    if(req.params.msg[0] == '@'){
+        var liMsg = req.params.msg.slice(1);
+        console.log(liMsg);
+        res.redirect('/api/trans/'+liMsg);
+    }else{
     var options = {
         method: 'GET',
         url: 'http://sandbox.api.simsimi.com/request.p',
@@ -31,6 +57,7 @@ router.get('/go/:msg', function(req, res, next) {
         console.log(response.headers);
         res.send(body);
       });
+    }
 });
 
 module.exports = router;
